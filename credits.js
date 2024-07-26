03e92069542bdf73e2b3c82d3ecc2c53fd0d17e3
dashuleshka <lakmand3@gmail.com>
2024-07-26 21:53:07 +0300

    rewrite sendPhpResponse, fixing README.md

--- credits.js
+++ credits.js
@@ -1,65 +1,88 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const btns = document.querySelectorAll(".apply-button");
-    const modalWindow = document.getElementById("modal");
-    const nameField = document.getElementById("name");
-    const emailField = document.getElementById("email");
-    const phoneField = document.getElementById("phone");
-    const formButton = document.querySelector('#form button[type="submit"]');
-    const closeButton = document.querySelector(".close");
-  
-    formButton.disabled = true;
-  
-    function checkFormValidity() {
-      formButton.disabled = !(
-        nameField.value.trim() &&
-        emailField.value.trim() &&
-        phoneField.value.trim()
-      );
-    }
+  const btns = document.querySelectorAll(".apply-button"); //buttons "Оформить заявку"
+  const modalWindow = document.getElementById("modal");
+  const nameField = document.getElementById("name");
+  const emailField = document.getElementById("email");
+  const phoneField = document.getElementById("phone");
+  const formButton = document.querySelector('#form button[type="submit"]');
+  const closeButton = document.querySelector(".close");
 
-    nameField.addEventListener("input", checkFormValidity);
-    emailField.addEventListener("input", checkFormValidity);
-    phoneField.addEventListener("input", checkFormValidity);
+  //button disabled пока все поля не заполнены
+  formButton.disabled = true;
 
-    btns.forEach(function (btn) {
-        btn.addEventListener("click", function () {
-        modalWindow.classList.remove("hidden-class");
-        });
+  function checkFormValidity() {
+    formButton.disabled = !(
+      nameField.value.trim() &&
+      emailField.value.trim() &&
+      phoneField.value.trim()
+    );
+  }
+
+  nameField.addEventListener("input", checkFormValidity);
+  emailField.addEventListener("input", checkFormValidity);
+  phoneField.addEventListener("input", checkFormValidity);
+
+  // Открытие модального окна при нажатии на кнопку
+  btns.forEach(function (btn) {
+    btn.addEventListener("click", function () {
+      modalWindow.classList.remove("hidden-class");
     });
-
-    closeButton.addEventListener("click", () => {
-        modalWindow.classList.add("hidden-class");
-    });
-
-    window.addEventListener("click", function (event) {
-        if (event.target === modalWindow) {
-        modalWindow.classList.add("hidden-class");
-    }
-        });
-
-    document.getElementById("form").addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    const formData = new FormData(this);
-
-    fetch("apply.php", {
-      method: "POST",
-      body: formData,
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.status === "success") {
-          alert(data.message);
-          console.log(data);
-          modalWindow.classList.add("hidden-class");
-          document.getElementById("form").reset();
-          formButton.disabled = true;
-        } else {
-          alert(data.message);
-        }
-      })
-      .catch((error) => {
-        alert(`Произошла ошибка при отправке данных: ${error.message}`);
-      });
   });
+
+  // Закрытие модального окна
+  closeButton.addEventListener("click", () => {
+    modalWindow.classList.add("hidden-class");
+  });
+
+  // Закрыть модальное окно при клике вне его содержимого
+  window.addEventListener("click", function (event) {
+    if (event.target === modalWindow) {
+      modalWindow.classList.add("hidden-class");
+    }
+  });
+
+  // вывод данных в консоль при отправке формы(задание 2)
+  const getFormResponse = async () => {
+    form.addEventListener("submit", async (event) => {
+      event.preventDefault();
+      console.log(
+        `Name: ${nameField.value}, Email: ${emailField.value}, Phone: ${phoneField.value}`
+      );
+    });
+  };
+
+  getFormResponse();
+
+  // функция отправки AJAX запроса из фронтенда на apply.php при отправке формы заявки
+  const sendPhpResponse = async () => {
+    document
+      .getElementById("form")
+      .addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const formData = new FormData(this);
+
+        fetch("apply.php", {
+          method: "POST",
+          body: formData,
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.status === "success") {
+              alert(data.message);
+              console.log(data);
+              modalWindow.classList.add("hidden-class");
+              document.getElementById("form").reset();
+              formButton.disabled = true;
+            } else {
+              alert(data.message);
+            }
+          })
+          .catch((error) => {
+            alert(`Произошла ошибка при отправке данных: ${error.message}`);
+          });
+      });
+  };
+
+  sendPhpResponse();
 });
